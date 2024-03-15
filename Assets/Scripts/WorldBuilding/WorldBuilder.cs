970a98eef99e19fad830c1dd992e1f4f94f6c508
RapsberryJam <163525629+RapsberryJam@users.noreply.github.com>
2024-03-15 12:52:46 +0100

    Added base logic to build world from chunks

---
+++ Assets/Scripts/WorldBuilding/WorldBuilder.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WorldBuilding
+{
+    public class WorldBuilder : MonoBehaviour
+    {
+        [field: SerializeField]
+        public List<Chunk> ChunkLibrary = new List<Chunk>();
+
+        [SerializeField]
+        private int desiredChunkCount = 5;
+        [SerializeField]
+        Transform worldStartPoint;
+
+        List<Chunk> activeChunks = new List<Chunk>();
+
+        private void Awake()
+        {
+            BuildWorld();
+        }
+
+        private void BuildWorld()
+        {
+            var firstChunk = GetRandomChunk();
+            ConnectChunk(worldStartPoint, firstChunk);
+
+            for (int i = 1; i < desiredChunkCount; i++)
+            {
+                var newChunk = GetRandomChunk();
+                ConnectChunk(activeChunks.Last().EndConnectionPoint, newChunk);
+            }
+        }
+
+        private Chunk GetRandomChunk()
+        {
+            return Instantiate(ChunkLibrary[Random.Range(0, ChunkLibrary.Count)], transform);
+        }
+
+        private void ConnectChunk(Transform connectionPoint, Chunk chunkToConnect)
+        {
+            chunkToConnect.transform.rotation = connectionPoint.rotation;
+
+            Vector3 offset = connectionPoint.position - chunkToConnect.StartConnectionPoint.position;
+            chunkToConnect.transform.position += offset;
+
+            activeChunks.Add(chunkToConnect);
+        }
+    }
+}
